0a9ba7c4896f4ed6dbc23b84c47c3e8aa2056295
AVN <nachigin88@mail.ru>
2020-04-18 21:48:46 +0800

    добавлена реализаци LoginPage

---
+++ pages/login_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in browser.current_url, "слово \"login\" not in url"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login FORM is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*MainPageLocators.REGISTER_FORM), "Register FORM is not presented"
