a3e00b39b59e4074d68782e7d5df9d9c079e4251
AVN <nachigin88@mail.ru>
2020-05-01 13:28:12 +0800

    task 4.2 step 9 - реализован второй вариант с явной инициализацией странице в теле теста при переходах между страницами

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -8,12 +9,12 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert "login" in browser.current_url, "слово \"login\" not in url"
+        assert "login" in self.browser.current_url, "слово login not in url"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login FORM is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login FORM is not presented"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*MainPageLocators.REGISTER_FORM), "Register FORM is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register FORM is not presented"
