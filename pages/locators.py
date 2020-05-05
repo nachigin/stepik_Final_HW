6a03a6d0d0fd674f660010c79fd6f0044250f79b
AVN <nachigin88@mail.ru>
2020-05-05 16:38:49 +0800

    task 4_3 step 4 поиск бага с параметризацией

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#id_login-username")
     REGISTER_FORM = (By.CSS_SELECTOR, "#id_registration-email")
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.XPATH, "//*[@id='add_to_basket_form']/button")
+    SUCCESS_MESAGE = (By.CSS_SELECTOR, '#messages>div:nth-child(1) strong')
+    PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
+    BASKET_TOTAL = (By.CSS_SELECTOR, '.alertinner p strong')
+
---
+++ pages/product_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+#import time
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        login_link = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        login_link.click()
+        #time.sleep(1)
+
+    def should_be_product_added_to_basket_by_name(self):
+        #реализует проверку что товар добавлен в корзину, проверка имени в сообщение
+        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESAGE), ('Success message is not present')
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        in_message_name = self.browser.find_element(*ProductPageLocators.SUCCESS_MESAGE).text
+        assert product_name == in_message_name, f'"{product_name}" is not "{in_message_name}"'
+        #time.sleep(1)
+
+    def should_be_product_added_to_basket_by_cost(self):
+        # реализует проверку что товар добавлен в корзину, проверка цены в корзине
+        assert self.is_element_present(*ProductPageLocators.BASKET_TOTAL), ('Total cost message is not ptesent')
+        product_prise = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        basket_cost = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL).text
+        assert product_prise == basket_cost, f'{product_prise} is not {basket_cost}'
+        #time.sleep(1)
